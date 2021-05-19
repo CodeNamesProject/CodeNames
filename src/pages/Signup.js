ee562b18192ea1e0b2c01e13864a6c87cf1ef96e
luthien017 <esmerkhalilli@gmail.com>
2021-05-20 00:22:12 +0400

    text fields for sign up page created

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,22 @@
+import Button from '../components/Button'
+import Header from '../components/Header'
 const Signup = () => {
     return (
         <div>
-           <h1>Sign up</h1> 
+            <form>
+                <Header title="Log in" size={75} family={'Thasadith'} />
+                <div>
+                    <label>Username</label>
+                    <input type='text' />
+                </div>
+                <br/>
+                <div>
+                    <label>Password</label>
+                    <input type='password' />
+                </div>
+                <br/>
+                <Button text='Log in' width={100} />
+            </form>
         </div>
     )
 }
