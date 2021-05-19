ec9fdb564d4e241f72c5c6a77efa614be9cebcc6
luthien017 <esmerkhalilli@gmail.com>
2021-05-20 00:19:59 +0400

    creation of pages and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Mainpage from './pages/Mainpage'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/" component={Mainpage} />
+        </Switch>
+      </div>
+      </Router>
   );
 }
 
