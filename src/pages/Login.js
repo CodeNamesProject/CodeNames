f6f3caf20d6a3030b6eeab2df2bef79d23597dc1
luthien017 <esmerkhalilli@gmail.com>
2021-05-29 22:38:21 +0400

    forms are updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,15 +8,15 @@ const Login = () => {
             
                 <Header title=" Log in" size={75} family={'Thasadith'} />
                 <div>
-                    <label>Username</label>
-                    <br/>
-                    <input type='text' className='input'/>
+                    {/* <label>Username</label>
+                    <br/> */}
+                    <input type='text' className='input' placeholder="Username"/>
                 </div>
                 <br/>
                 <div>
-                    <label>Password</label>
-                    <br/>
-                    <input type='password' className='input'/>
+                    {/* <label>Password</label>
+                    <br/> */}
+                    <input type='password' className='input' placeholder="Password"/>
                 </div>
                 <br/>
                 <Button text='Log in' width={253} nav='/homepage' />
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,33 +10,33 @@ const Signup = () => {
                 <Header title="Sign up" size={75} family={'Thasadith'} />
                 <div className="content">
                     <div>
-                        <label>First Name</label>
-                        <br/>
-                        <input type='text' className='input' name='firstname' />
+                        {/* <label>First Name</label>
+                        <br/> */}
+                        <input type='text' className='input' name='firstname' placeholder="First Name" />
                     </div>
                     <br/>
                     <div>
-                        <label>Last Name</label>
-                        <br/>
-                        <input type='text' className='input' name='lastname' />
+                        {/* <label>Last Name</label>
+                        <br/> */}
+                        <input type='text' className='input' name='lastname' placeholder="Last Name" />
                     </div>
                     <br/>
                     <div>
-                        <label>User Name</label>
-                        <br/>
-                        <input type='text' className='input' name='username' />
+                        {/* <label>User Name</label>
+                        <br/> */}
+                        <input type='text' className='input' name='username' placeholder="User Name" />
                     </div>
                     <br/>
                     <div>
-                        <label>Email</label>
-                        <br/>
-                        <input type='email' className='input' name='email' />
+                        {/* <label>Email</label>
+                        <br/> */}
+                        <input type='email' className='input' name='email' placeholder="Email" />
                     </div>
                     <br/>
                     <div>
-                        <label>Password</label>
-                        <br/>
-                        <input type='password' className='input' name='password' />
+                        {/* <label>Password</label>
+                        <br/> */}
+                        <input type='password' className='input' name='password' placeholder="Password" />
                     </div>
                 </div>
                 <br/>
